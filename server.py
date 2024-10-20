25528b488c3ebdd8040638004194638ab2348b30
Jimmy Tran <92623786+jimbucktwo@users.noreply.github.com>
2024-10-20 14:19:44 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,24 @@
 import socket
 import random
 
+# socket created
 TCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#gets the IP address of the server
 hostname = socket.gethostname()
 IPaddress = socket.gethostbyname(hostname)
+
+# assigns a random port number
 port = random.randint(1024, 49151)
+
+#binds the IP and port to the socket and listens to the client
 TCPSocket.bind((IPaddress, port)) 
 TCPSocket.listen(5)
 print(f"IP address of server : {IPaddress}\nPort Number : {port}\nServer listening...")
 
 incomingSocket, incomingAdress = TCPSocket.accept()
 
+# infinite loop to listen to the client and send data back until the client closes the connection
 while True:
     myData = incomingSocket.recv(1024).decode()
     if myData:
