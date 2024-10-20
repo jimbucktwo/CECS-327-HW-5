c4d39854c357990295c019f270235a8439807834
Jimmy Tran <92623786+jimbucktwo@users.noreply.github.com>
2024-10-20 14:18:34 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
 import socket
 
+# creates a socket
 TCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# prompts the user to enter an IP address and port number, keeps trying until it successfully connects
 while True:
     try:
         serverIP = str(input("Enter IP address: "))
@@ -12,14 +14,16 @@ while True:
         print("Either the IP address or port number was entered incorrectly, try again.")
 
 
+# infinite loop so the user can send as many messages as they want
 while True:
     message = input("Enter message you want to send (Enter to exit): ")
     if message:
+        # sends the message to the server
         TCPSocket.sendall(message.encode())
         serverResponse = TCPSocket.recv(1024)
         print(f'Server Response: "{serverResponse.decode()}"')
     else:
         break
 
-
+#closes the socket
 TCPSocket.close()
